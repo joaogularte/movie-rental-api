9348350198d9319b4a3c5587422905a42b1c93c4
joaogularte <jov96@hotmail.com>
2019-01-05 17:07:02 -0200

    :)

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,9 @@ const config = {
         host: process.env.DB_HOST,
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 }
-const knex = require('knex');
-module.exports = knex;
+const knex = require('knex')(config);
+module.exports = knex; 
 
