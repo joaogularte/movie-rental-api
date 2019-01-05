0100521b4aef87f17a5cf7e44be76a0d2087107d
joaogularte <jov96@hotmail.com>
2019-01-05 02:54:19 -0200

    Desenvolvido model Rental

---
+++ src/models/RentalModel.js
@@ -0,0 +1,44 @@
+const knex = require('../config/db');
+const rentalStatus = require('./status/rental.json');
+
+class RentalModel{
+    static list(){
+        return knex
+                .select('id', 'movies.title', 'user.name', 'user.email', 'status')
+                .from('rentals')
+                .innerJoin('movies', 'rentals.idMovie', 'movies.id')
+                .innerJoin('users', 'rentals.idUser', 'users.id');
+    }
+
+    static get(rentalId){
+        return knex
+                .select('id', 'movies.title', 'user.name', 'user.email')
+                .from('rentals')
+                .innerJoin('movies', 'rentals.idMovie', 'movies.id')
+                .innerJoin('users', 'rentals.idUser', 'users.id')
+                .where('id', rentalId);
+    }
+
+    static post(data){
+        return knex.from('rentals').insert(data);
+    }
+
+    static put(rentalId, data){
+        const query = knex.from('rentals');
+
+        if(data.status){
+            query.update('status', rentalStatus.returned)
+        }
+
+        return query.where('id', rentalId);
+    }
+
+    static delete(rentalId){
+        return knex
+            .from('rentals')
+            .where('id', rentalId)
+            .del();
+    }
+}
+
+module.exports = RentalModel;
