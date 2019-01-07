5b9deb5244bf4a55b34ec2c9da182742c13942f3
joaogularte <jov96@hotmail.com>
2019-01-07 00:50:33 -0200

    Teste unitarios movies OK

--- test/unit/services/movies.js
+++ test/unit/services/movies.js
@@ -2,34 +2,59 @@ const MovieService = require('../../../src/services/MovieService');
 
 describe('Services Movies', () => {
     
-    const defaultMovie = {
+    const defaultMovie = [{
         id: 'd1b039e8-997b-4bdf-8a20-1fae7c1c7c0d',
         title: 'Default Movie',
         director: 'Director of Default Movie',
         quantities: 1
+    }];
+
+    const updatedMovie = {
+        title: 'Updated Movie',
+        director: 'Director Updated',
+        quantities: 2
     }
 
     beforeEach(done => {
         knex.from('movies').del().then(() => {
-            knex.from('movies').insert(defaultMovie).then(() => {
+            knex.from('movies').insert(defaultMovie[0]).then(() => {
                 done();
             })
         })
     });
     
     describe('Get all movies: list()', () => {
-        it('should return a list of movies', () => {
+        it('should return a list of movies', async () => {
+            const movies = await MovieService.list();
+            expect(movies).to.be.eql(defaultMovie);
+        })
+    });
+
+    describe('Get a movie: get()', () => {
+        it('should return a movie', async () => {
+            const movie = await MovieService.get(defaultMovie[0].id)
+            expect(movie).to.be.eql(defaultMovie[0]);
+        })
+    });
+
+    describe('Create a movie: post()', () => {
+        it('should return a movie id', async () => {
+            const movieId = await MovieService.post(defaultMovie[0])
+            expect(movieId).to.have.lengthOf(36);
+        })
+    })
+
+    describe('Update a movie: put()', () => {
+        it('should return true', async () => {
+            const updated = await MovieService.put(defaultMovie[0].id, updatedMovie);
+            expect(updated).to.have.eql(true);
+        })
+    })
+    describe('Create a movie: delete()', () => {
+        it('should return true', async () => {
+            const deleted = await MovieService.delete(defaultMovie[0].id)
+            expect(deleted).to.have.eql(true);
         
-            const expetedMovie = [{
-                id: 'd1b039e8-997b-4bdf-8a20-1fae7c1c7c0d',
-                title: 'Default Movie',
-                director: 'Director of Default Movie',
-                quantities: 1
-            }]
-            MovieService.list()
-                .then(movies => {
-                    expect(movies).to.be.eql(expetedMovie);
-                })
         })
     })
 });
