07b32a729882704889955017c1701f2778a6ca61
joaogularte <jov96@hotmail.com>
2019-01-07 02:52:48 -0200

    Adicionado user

---
+++ src/models/UserModel.js
@@ -0,0 +1,43 @@
+const knex = require('../config/db');
+
+class UserModel{
+    static get(userId){
+        return knex
+            .select('id', 'name', 'email', 'password', 'role')
+            .from('users')
+            .where('id', userId);
+    }
+
+    static post(data){
+        return knex('users')
+            .insert(data);
+    }
+
+    static put(userId, data){
+        const query = knex.from('users');
+
+        if(data.name){
+            query.update('name', data.name);
+        }
+        if(data.email){
+            query.update('email', data.email);
+        }
+        if(data.password){
+            query.update('password', data.password);
+        }
+        if(data.role){
+            query.update('role', data.role);
+        }
+
+        return query.where('id', userId);
+    }
+
+    static delete(userId){
+        return knex
+            .from('users')
+            .where('id', userId)
+            .del();
+    }
+}
+
+module.exports = UserModel;
---
+++ src/services/UserService.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const uuidv4 = require('uuid/v4');
+const UserModel = require('../models/UserModel');
+
+class UserService{
+    static async get(userId){
+        const user = await UserModel.get(userId);
+        if(!user){
+            return null
+        }
+        return user[0];
+    }
+
+    static async post(data){
+        const id = uuidv4();
+
+        const salt = bcrypt.genSaltSync();
+
+        const password = bcrypt.hashSync(data.password, salt);
+        const user = {
+            id: id,
+            name: data.name,
+            email: data.email,
+            password: password,
+            role: data.role
+        }
+        await UserService.post(user);
+        return user.id;
+    }
+
+    static async put(userId, data){
+        const user = await UserModel.get(userId);
+        if(!user){
+            return false;
+        }
+        await UserModel.put(userId, data);
+        return true;
+    }
+
+    static async delete(userId){
+        const user = await UserModel.get(userId);
+        if(!user){
+            return false;
+        }
+        await UserModel.delete(userId);
+        return true;
+    }
+
+    static async isPassword(userId, password){
+        const user = await UserModel.get(userId);
+        if(!user){
+            return false;
+        }
+
+        const result = bcrypt.compareSync(password, user[0].password);
+        return result;
+    }
+
+
+}
+
+module.exports = UserService;
