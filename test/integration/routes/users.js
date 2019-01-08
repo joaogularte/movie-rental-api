1fae9bfc0f0a3f1a8efb303f2b2204059191a007
joaogularte <jov96@hotmail.com>
2019-01-08 03:34:46 -0200

    Desenvolvido teste integração user

--- test/integration/routes/users.js
+++ test/integration/routes/users.js
@@ -12,7 +12,10 @@ describe('Routes Users', () => {
     beforeEach(done => {
         knex.from('users').del().then(() => {
             UserService.post(defaultUser)
-                .then(() => done());
+                .then((userId) => {
+                    defaultUser.id = userId.id;
+                    done()
+                });
         })
     });
 
@@ -21,26 +24,79 @@ describe('Routes Users', () => {
             request
                 .get('/api/users')
                 .end((err, res) => {
-                    
+
+                    expect(res.body.data[0].id).to.have.lengthOf(36);
                     expect(res.body.data[0].name).to.be.eql(defaultUser.name);
                     expect(res.body.data[0].email).to.be.eql(defaultUser.email);
                     expect(res.body.data[0].role).to.be.eql(defaultUser.role);
+                    
                     done(err);
                 })
         })
     });
 
     describe('Route GET /api/users/{id}', () => {
-        it('should return a movie', done => {
+        it('should return a user', done => {
             request
-                .get('/api/users/d1b039e8-997b-4bdf-8a20-1fae7c1c7c0d')
+                .get(`/api/users/${defaultUser.id}`)
                 .end((err, res) => {
 
-                    expect(res.body.data.id).to.be.eql(defaultMovie.id);
-                    expect(res.body.data.title).to.be.eql(defaultMovie.title);
-                    expect(res.body.data.director).to.be.eql(defaultMovie.director);
-                    expect(res.body.data.quantities).to.be.eql(defaultMovie.quantities);
+                    expect(res.body.data.id).to.be.eql(defaultUser.id);
+                    expect(res.body.data.name).to.be.eql(defaultUser.name);
+                    expect(res.body.data.email).to.be.eql(defaultUser.email);
+                    expect(res.body.data.role).to.be.eql(defaultUser.role);
 
+                    done(err);
+                })
+        })
+    });
+
+    describe('Route POST /api/users', () => {
+        it('should create a user', done => {
+
+            const newUser = {
+                name: 'New User',
+                email: 'newuser@email.com',
+                password: 'supersenha'
+            }
+
+            request
+                .post('/api/users')
+                .send(newUser)
+                .end((err, res) => {
+
+                    expect(res.body.data.id).to.have.lengthOf(36);
+                    
+                    done(err);
+                })
+        })
+    });
+
+    describe('Route PUT /api/users/{id}', () => {
+        it('should edit a user', done => {
+            const updatedUser = {
+                name: 'Updated User',
+                email: 'Director of Updated Movie',
+                password: 'senha'
+            }
+
+            request
+                .put(`/api/users/${defaultUser.id}`)
+                .send(updatedUser)
+                .end((err, res) => {
+                    expect(res.body.success).to.be.eql(true);
+                    done(err);
+                })
+        })  
+    });
+
+    describe('Route DELETE /api/users/{id}', () => {
+        it('should delete a user', done => {
+
+            request
+                .delete(`/api/users/${defaultUser.id}`)
+                .end((err, res) => {
+                    expect(res.body.success).to.be.eql(true);
                     done(err);
                 })
         })
