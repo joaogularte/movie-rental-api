e3936b1fa5d5409e63a47b48eae121fb53017bea
joaogularte <jov96@hotmail.com>
2019-01-08 03:33:36 -0200

    Adicionado metodos para as rotas

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,4 +4,12 @@ const UserController = require('../controllers/UserController');
 
 router.get('/', UserController.list);
 
+router.post('/', UserController.post);
+
+router.get('/:id', UserController.get);
+
+router.put('/:id', UserController.put);
+
+router.delete('/:id', UserController.delete);
+
 module.exports = router;
