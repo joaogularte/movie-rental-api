41225668c583c07e092f4e0237912a83fff99ed8
joaogularte <jov96@hotmail.com>
2019-01-10 01:53:55 -0200

    Removido campo role

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,50 +1,55 @@
 const knex = require('../config/db');
 
-class UserModel{
+class UserModel {
+  static list() {
+    return knex
+      .select('id', 'name', 'email', 'password')
+      .from('users');
+  }
 
-    static list(){
-        return knex
-            .select('id', 'name', 'email', 'password', 'role')
-            .from('users');
+  static get(userId) {
+    return knex
+      .select('id', 'name', 'email', 'password')
+      .from('users')
+      .where('id', userId);
+  }
+
+  static post(data) {
+    return knex('users')
+      .insert(data);
+  }
+
+  static put(userId, data) {
+    const query = knex.from('users');
+
+    if (data.name) {
+      query.update('name', data.name);
+    }
+    if (data.email) {
+      query.update('email', data.email);
+    }
+    if (data.password) {
+      query.update('password', data.password);
     }
 
-    static get(userId){
-        return knex
-            .select('id', 'name', 'email', 'password', 'role')
-            .from('users')
-            .where('id', userId);
-    }
+    return query.where('id', userId);
+  }
 
-    static post(data){
-        return knex('users')
-            .insert(data);
-    }
+  static delete(userId) {
+    return knex
+      .from('users')
+      .where('id', userId)
+      .del();
+  }
 
-    static put(userId, data){
-        const query = knex.from('users');
+  /* Metodos Extras */
+  static getByEmail(userEmail){
+    return knex
+      .select('id')
+      .from('users')
+      .where('email', userEmail);
+  }
 
-        if(data.name){
-            query.update('name', data.name);
-        }
-        if(data.email){
-            query.update('email', data.email);
-        }
-        if(data.password){
-            query.update('password', data.password);
-        }
-        if(data.role){
-            query.update('role', data.role);
-        }
-
-        return query.where('id', userId);
-    }
-
-    static delete(userId){
-        return knex
-            .from('users')
-            .where('id', userId)
-            .del();
-    }
 }
 
 module.exports = UserModel;
