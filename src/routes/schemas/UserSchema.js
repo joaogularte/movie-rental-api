f04d966b5603e26a14caa0212f1024ff46320175
joaogularte <jov96@hotmail.com>
2019-01-10 01:54:45 -0200

    Removido role

--- src/routes/schemas/UserSchema.js
+++ src/routes/schemas/UserSchema.js
@@ -1,57 +1,52 @@
-const Joi = require('Joi');
+const Joi = require('joi');
 const Schema = require('./Schema');
 
 
 class UserSchema extends Schema {
-    static get(data) {
-        const schema = {
-            params: Joi.object({
-                id: Joi.string().guuid({version: ['uuidv4']}).trim().required(),
-            })
-        }
-        return this.validate(data, schema)
-    }
+  static get(data) {
+    const schema = {
+      params: Joi.object({
+        id: Joi.string().guid({ version: ['uuidv4'] }).trim().required(),
+      }),
+    };
+    return this.validate(data, schema);
+  }
 
-    static list(data) {
-    }
 
-    static post(data) {
-        const schema = {
-            body: Joi.object({
-                name: Joi.string().trim().required(),
-                email: Joi.string().email().trim().required(),
-                passwrod: Joi.string().trim().required(),
-                role: Joi.string().valid(['admin', 'user']).required()
-            })
-        }
-        return this.validate(data, schema)
-    }
+  static post(data) {
+    const schema = {
+      body: Joi.object({
+        name: Joi.string().trim().required(),
+        email: Joi.string().email().trim().required(),
+        password: Joi.string().trim().required(),
+      }),
+    };
+    return this.validate(data, schema);
+  }
 
-    static put(data) {
-        const schema = {
-            params: Joi.object({
-                id: Joi.string().guuid({version: ['uuidv4']}).required(),
-            }),
-            body: Joi.object({
-                name: Joi.string().trim().required(),
-                email: Joi.string().email().trim().required(),
-                passwrod: Joi.string().trim().required(),
-                role: Joi.string().valid(['admin', 'user']).required()
-            })
-        }
-        return this.validate(data, schema);
-    }
+  static put(data) {
+    const schema = {
+      params: Joi.object({
+        id: Joi.string().guid({ version: ['uuidv4'] }).required(),
+      }),
+      body: Joi.object({
+        name: Joi.string().trim().required(),
+        email: Joi.string().email().trim().required(),
+        password: Joi.string().trim().required(),
+      }),
+    };
+    return this.validate(data, schema);
+  }
 
-    static delete(data) {
-        const schema = {
-            params: Joi.object({
-                id: Joi.string().guuid({version: ['uuidv4']}).trim().required()
-            })
-        }
-        return this.validate(data, schema);
-    }
+  static delete(data) {
+    const schema = {
+      params: Joi.object({
+        id: Joi.string().guid({ version: ['uuidv4'] }).trim().required(),
+      }),
+    };
+    return this.validate(data, schema);
+  }
 }
-
 
 
 module.exports = UserSchema;
