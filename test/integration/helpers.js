1d594aaba1b8ff1262993b1477b8aec98a3f93e0
joaogularte <jov96@hotmail.com>
2019-01-15 22:21:23 -0200

    Adicionado knex ao helpers

--- test/integration/helpers.js
+++ test/integration/helpers.js
@@ -1,7 +1,9 @@
 const supertest = require('supertest');
 const chai = require('chai');
 const app = require('../../src/app');
+const knex = require('../../src/config/db');
 
 global.app = app;
 global.expect = chai.expect;
 global.request = supertest(app);
+global.knex = knex;
