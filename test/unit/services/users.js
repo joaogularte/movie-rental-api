cf4b89f99229f2ea9b8e0e9257b5c7759435be41
joaogularte <jov96@hotmail.com>
2019-01-17 22:44:10 -0200

    Adicionado primeiros testes

---
+++ test/unit/services/users.js
@@ -0,0 +1,36 @@
+const UserService = require('../../../src/services/UserService');
+
+describe('Services Users', () => {
+  const defaultUser = [{
+    id: '27ff9681-ca75-43ca-91b1-a10447943395',
+    name: 'Default User',
+    email: 'defaultuser@email.com',
+    password: 'senhaDefaultUser',
+  }];
+
+  beforeEach((done) => {
+    knex.from('users')
+      .insert(defaultUser[0])
+      .then(() => done());
+  });
+
+  afterEach((done) => {
+    knex.from('users')
+      .del()
+      .then(() => done());
+  });
+
+  describe('Get all users: list()', () => {
+    it('should return a list of users', async () => {
+      const users = await UserService.list();
+      expect(users).to.be.eql(defaultUser);
+    });
+  });
+
+  describe('Get an user: get()', () => {
+    it('should return an user', async () => {
+      const user = await UserService.get(defaultUser[0].id);
+      expect(user).to.be.eql(defaultUser[0]);
+    });
+  });
+});
