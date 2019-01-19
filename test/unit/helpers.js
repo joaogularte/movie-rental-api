1229a0728e0b8a5177be3e888b719728f12e722e
joaogularte <jov96@hotmail.com>
2019-01-19 14:36:13 -0200

    Adicionado chai-uuid ao chai

--- test/unit/helpers.js
+++ test/unit/helpers.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const chai = require('chai');
 const knex = require('../../src/config/db');
+chai.use(require('chai-uuid'));
 
 global.expect = chai.expect;
 global.knex = knex;
