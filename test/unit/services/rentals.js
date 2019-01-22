e7525569e2a3f6ff93eed87b9b43a4a38b6838c7
joaogularte <jov96@hotmail.com>
2019-01-22 21:03:42 -0200

    Adicionado testes para rentals service

--- test/unit/services/rentals.js
+++ test/unit/services/rentals.js
@@ -1,5 +1,16 @@
 const RentalService = require('../../../src/services/RentalService');
 
 describe('Services Rentals', () => {
-
+    const defaultRental = {
+        id: '14dbd0cf-56fd-46f8-a5eb-971c844f0024',
+        titleMovie: '',
+        idUser: '',
+        status: 'rented',
+    }
+    const defaultMovie = {
+        id: '801ca591-2a7f-404d-ba7e-4ad57801f9c1',
+        title: 'Default Movie',
+        director: 'Director of Default Movie',
+        quantities: 3    
+    }
 });
