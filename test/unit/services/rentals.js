01986129118af43c18462feb2df556512886786b
joaogularte <jov96@hotmail.com>
2019-02-19 09:32:23 -0300

    Added unit testing put() and post()

--- test/unit/services/rentals.js
+++ test/unit/services/rentals.js
@@ -19,7 +19,15 @@ describe('Services Rentals', () => {
     email: 'defaultuser@email.com',
     password: '12345',
   }];
-
+  const newRental = [{
+    id: '2626d9fb-6633-4f94-9995-1e11c7a69d76',
+    titleMovie: 'Default Movie',
+    idUser: '51f5fb36-db54-4e68-a0f1-634a7d82ac3c',
+    status: 'rented',
+  }];
+  const updatedRental = [{
+    status: 'returned',
+  }];
   beforeEach((done) => {
     knex.from('users')
       .insert(defaultUser[0])
@@ -69,6 +77,20 @@ describe('Services Rentals', () => {
       expect(rental).to.have.property('name');
       expect(rental).to.have.property('email');
       expect(rental).to.have.property('status');
+    });
+  });
+
+  describe('Create a rental: post()', () => {
+    it('should return a rental id', async () => {
+      const rentalId = await RentalService.post(newRental[0]);
+      expect(rentalId.data.id).to.be.a.uuid('v4');
+    });
+  });
+
+  describe('Update a rental: put()', () => {
+    it('should return true', async () => {
+      const updated = await RentalService.put(defaultRental[0].id, updatedRental[0]);
+      expect(updated).to.be.true;
     });
   });
 });
